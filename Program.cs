5494f879111d7ed2cc614d85760fafe4490b5f72
Petrov Nikolay <nikolay.petrov99@mail.ru>
2022-08-15 14:54:33 +0300

    second commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите трехзначное число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string threeNumbers = Convert.ToString(numbers);
+    if (threeNumbers.Length > 2)
+    {
+        Console.Write("Третья цифра -  "+ threeNumbers[2]);
+    }
+    else
+    {
+        Console.Write(" третьей цифры нет");
+    }
+
